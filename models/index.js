4decc3e26633a289ad5fae26d193eec49a83ecb1
VipulR2796 <113733843+VipulR2796@users.noreply.github.com>
2023-10-12 15:40:41 -0400

    Feature (#2) (#2)
    * Initial Commit
    
    * Added integration test and workflow
    
    * Updated workflow
    
    * Updated workflow
    
    * Updated workflow
    
    * Modified yml file
    
    * Modified yml file
    
    * fixed test issue
    
    * updated workflow
    
    * updated workflow
    
    * updated workflow- mysql parameters
    
    * testing workflow
    
    * updated config and workflow
    
    * workflow updated
    
    ---------
    
    Co-authored-by: Vipul Rajderkar <vipulrajderkar@Vipuls-MacBook-Pro.local>

--- models/index.js
+++ models/index.js
@@ -6,13 +6,20 @@ const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+// const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
+
+
+console.log(config);
 
 let sequelize;
 if (config.use_env_variable) {
+  console.log("ifffff");
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  
 } else {
+  console.log("elseeeeee");
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
