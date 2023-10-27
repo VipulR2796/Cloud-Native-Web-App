1da074dd8baabfd4da450e48c0cc0e978e2794a6
VipulR2796 <113733843+VipulR2796@users.noreply.github.com>
2023-10-26 20:55:13 -0400

    Fixed auto start issue (#13)
    Co-authored-by: Vipul Rajderkar <vipulrajderkar@Vipuls-MacBook-Pro.local>

--- scripts/install.sh
+++ scripts/install.sh
@@ -32,11 +32,13 @@ sudo unzip webapp.zip
 sudo cp csye6225.service /etc/systemd/system
 sudo chown -R csye6225:csye6225 /opt/csye6225
 sudo chmod +x /opt/csye6225/server.js
+sudo touch /opt/csye6225/.env
+sudo chown csye6225:csye6225 /opt/csye6225/.env
 
 sudo npm install
 
 sudo systemctl daemon-reload
 sudo systemctl enable csye6225
 sudo systemctl start csye6225
-sudo systemctl restart csye6225
-sudo systemctl stop csye6225
+# sudo systemctl restart csye6225
+# sudo systemctl stop csye6225
